3dba383c65dd9b041c49ac9499a19404c68f3bf6
Ian K Armstrong <ian.k.armstrong@gmail.com>
2024-07-22 16:06:58 +0200

    update

--- lib/search/search.dart
+++ lib/search/search.dart
@@ -4,6 +4,8 @@ import 'package:revelation/main/queries.dart';
 import 'package:revelation/utils/globals.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
+String contents = '';
+
 class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
 
@@ -16,53 +18,128 @@ class SearchPageState extends State<SearchPage> {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<List<Rev>>(
-      future: RevQueries().getSearchedValues('topaz'),
-      builder: (context, AsyncSnapshot<List<Rev>> snapshot) {
-        if (snapshot.hasData) {
-          list = snapshot.data!;
-          return Scaffold(
-            appBar: AppBar(
-              centerTitle: true,
-              elevation: 5,
-              title: Text(
-                AppLocalizations.of(context)!.search,
-                style: const TextStyle(fontWeight: FontWeight.w700),
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        elevation: 5,
+        title: Text(
+          AppLocalizations.of(context)!.search,
+          style: const TextStyle(fontWeight: FontWeight.w700),
+        ),
+        leading: GestureDetector(
+          child: const Icon(Icons.arrow_back),
+          onTap: () {
+            Future.delayed(
+              Duration(milliseconds: Globals.navigatorDelay),
+              () {
+                Navigator.of(context).pop();
+              },
+            );
+          },
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Search',
+                suffixIcon: Icon(Icons.search),
               ),
-              leading: GestureDetector(
-                child: const Icon(Icons.arrow_back),
-                onTap: () {
-                  Future.delayed(
-                    Duration(milliseconds: Globals.navigatorDelay),
-                    () {
-                      Navigator.of(context).pop();
-                    },
-                  );
+              onChanged: (value) {
+                contents = value;
+                debugPrint(contents);
+              },
+              
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Expanded(
+              child: FutureBuilder<List<Rev>>(
+                future: RevQueries().getSearchedValues('lamb'),
+                builder: (context, AsyncSnapshot<List<Rev>> snapshot) {
+                  if (snapshot.hasData) {
+                    list = snapshot.data!;
+                    return ListView.separated(
+                      itemCount: list.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return ListTile(
+                          title: Text(
+                            list[index].t,
+                            style: Theme.of(context).textTheme.bodyMedium,
+                          ),
+                        );
+                      },
+                      separatorBuilder: (context, index) {
+                        return const Divider();
+                      },
+                    );
+                  } else {
+                    return const CircularProgressIndicator();
+                  }
                 },
               ),
             ),
-            body: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ListView.separated(
-                itemCount: list.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return ListTile(
-                    title: Text(
-                      list[index].t,
-                      style: Theme.of(context).textTheme.bodyMedium,
-                    ),
-                  );
-                },
-                separatorBuilder: (context, index) {
-                  return const Divider();
-                },
-              ),
-            ),
-          );
-        } else {
-          return const CircularProgressIndicator();
-        }
-      },
+          ],
+        ),
+      ),
     );
   }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return FutureBuilder<List<Rev>>(
+  //     future: RevQueries().getSearchedValues('lamb'),
+  //     builder: (context, AsyncSnapshot<List<Rev>> snapshot) {
+  //       if (snapshot.hasData) {
+  //         list = snapshot.data!;
+  //         return Scaffold(
+  //           appBar: AppBar(
+  //             centerTitle: true,
+  //             elevation: 5,
+  //             title: Text(
+  //               AppLocalizations.of(context)!.search,
+  //               style: const TextStyle(fontWeight: FontWeight.w700),
+  //             ),
+  //             leading: GestureDetector(
+  //               child: const Icon(Icons.arrow_back),
+  //               onTap: () {
+  //                 Future.delayed(
+  //                   Duration(milliseconds: Globals.navigatorDelay),
+  //                   () {
+  //                     Navigator.of(context).pop();
+  //                   },
+  //                 );
+  //               },
+  //             ),
+  //           ),
+  //           body: Padding(
+  //             padding: const EdgeInsets.all(8.0),
+  //             child: ListView.separated(
+  //               itemCount: list.length,
+  //               itemBuilder: (BuildContext context, int index) {
+  //                 return ListTile(
+  //                   title: Text(
+  //                     list[index].t,
+  //                     style: Theme.of(context).textTheme.bodyMedium,
+  //                   ),
+  //                 );
+  //               },
+  //               separatorBuilder: (context, index) {
+  //                 return const Divider();
+  //               },
+  //             ),
+  //           ),
+  //         );
+  //       } else {
+  //         return const CircularProgressIndicator();
+  //       }
+  //     },
+  //   );
+  // }
 }
